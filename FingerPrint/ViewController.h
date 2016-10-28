bdad827fd3a462c110a316dd3c94683d113cd15a
Ethan He <tashaxing123@163.com>
2016-10-28 12:27:32 +0800

    add

---
+++ FingerPrint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FingerPrint
+//
+//  Created by yxhe on 16/10/28.
+//  Copyright © 2016年 tashaxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FingerPrint/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  FingerPrint
+//
+//  Created by yxhe on 16/10/28.
+//  Copyright © 2016年 tashaxing. All rights reserved.
+//
+
+// ---- 指纹识别测试 ---- //
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
